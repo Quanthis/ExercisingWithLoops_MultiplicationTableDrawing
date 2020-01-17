15595a42c4430089cabb55aa89aceca43f225059
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-17 15:10:18 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//3.9
+            int j;
+            for (int i=1; i<=10; i++)
+            {
+                for (j = 1; j <= 10; j++)
+                    Console.Write("{0, 3} ", j*i);
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
